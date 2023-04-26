9eddcd6ea3b3e257220a8832d7bcf79d8f511cc0
Marcos Luiz Wilhelm <marcoswilhelm@gmail.com>
2023-04-26 15:30:06 -0300

    Adicionado suporte a mais um stream como por exemplo uma webcam.

--- README.md
+++ README.md
@@ -10,6 +10,10 @@ Modelos RTSP:
 * #rtsp://[usuario]:[senha]@[ip]:554/Streaming/Channels/101
 * #rtsp://[usuario]:[senha]@[ip]:554/Streaming/Channels/102
 
+Modelos webcam:
+* v4l2:///dev/video2
+* Obs.: o comando acima obtém a webcam visível pelo sistema. Para utilizar smartphone, pode-se utilizar o app droidcam em conjunto. Instalação em: https://diolinux.com.br/aplicativos/droidcam-webcam-com-o-smartphone.html
+
 Execução:
 1) Crie um arquivo .env utilizando o arquivo .env.example com exemplo.
 2) Execute "source execute_rtsp_mosaic.sh"
--- execute_rtsp_mosaic.sh
+++ execute_rtsp_mosaic.sh
@@ -20,22 +20,27 @@ new ch3 broadcast enabled
 setup ch3 input $STREAM03
 setup ch3 output #mosaic-bridge{id=ch3,width=640,height=540}
 
+new ch4 broadcast enabled
+setup ch4 input $STREAM04
+setup ch4 output #mosaic-bridge{id=ch4,width=640,height=540}
+
 new bg broadcast enabled
 # The following background file is simply a black picture with specified dimensions, you can create one with any image editor, like mspaint, it is required for proper mosaic size
 setup bg input "bg.jpg"
 setup bg option image-duration=-1
 
-setup bg option image-fps=15
+setup bg option image-fps=30
 setup bg option mosaic-width=1920
 setup bg option mosaic-height=1080
 setup bg option mosaic-keep-picture=1	
 
-setup bg output #transcode{vcodec=mp4v,vb=0,fps=0,acodec=none,channels=3,sfilter=mosaic{alpha=255,width=1280,height=1080,cols=2,rows=2,position=1,order="ch1,ch2,ch3",keep-aspect-ratio=enabled,mosaic-align=0,keep-picture=1}}:bridge-in{offset=100}:display
+setup bg output #transcode{vcodec=mp4v,vb=0,fps=0,acodec=none,channels=4,sfilter=mosaic{alpha=255,width=1280,height=1080,cols=2,rows=2,position=1,order="ch1,ch2,ch3,ch4",keep-aspect-ratio=enabled,mosaic-align=0,keep-picture=1}}:bridge-in{offset=100}:display
 
 control bg play
 control ch1 play
 control ch2 play
 control ch3 play
+control ch4 play
 
 EOF
 
